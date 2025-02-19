33b9215987f927951e6dd2d9fb7cca703d9b5597
Brian Winn <bwinn@usc.edu>
2025-02-19 07:15:50 +0000

    part 1 finished

--- split.cpp
+++ split.cpp
@@ -18,6 +18,32 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  //Base case: if input list is empty, return immediately; Ends recursion when input list is emptied
+  if (in == nullptr){
+    return;
+  }
+
+  //keeps track of current node of "in" in temporary pointer; 
+  //set head of in to the next node in list, ensures recursive calls act on remainder of list
+  Node* temp = in;
+  in = in->next;
+
+  //if current node is even, adds node to evens list and calls split recursively
+  //updates evens->next upon recursive call
+  if (temp->value % 2 == 0){
+    evens = temp;
+    evens->next = nullptr;
+    split(in, odds, evens->next);
+  } 
+
+  //if current node is odd, adds node to odds list and calls split recursively
+  //updates odds->next upon recursive call
+  else {
+    odds = temp;
+    odds->next = nullptr;
+    split(in, odds->next, evens);
+  }
+   
 }
 
 /* If you needed a helper function, write it here */
